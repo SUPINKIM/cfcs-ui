99e8233e3eae1d8389cdf990a1f2de60f39634bf
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2023-03-01 21:26:04 +0900

    feat: eslint 규칙 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,82 @@
 module.exports = {
-    "extends": "standard",
-    "rules": {
-        "indent": ["error", 4],
-        "semi": ["error", "always"],
-        "no-trailing-spaces": 0,
-        "keyword-spacing": 0,
-        "no-unused-vars": 1,
-        "no-multiple-empty-lines": 0,
-        "space-before-function-paren": 0,
-        "eol-last": 0
+  root: true,
+  'parser': '@typescript-eslint/parser',
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'extends': [
+    'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended'
+  ],
+  'rules': {
+    'max-len': [
+      'error', 120
+    ],
+    'indent': [
+      'error', 2
+    ],
+    'semi': [
+      'error', 'always'
+    ],
+    'no-trailing-spaces': 'error',
+    'keyword-spacing': 'error',
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        'max': 1,
+        'maxEOF': 0
+      }
+    ],
+    'space-before-function-paren': 'error',
+    'curly': [
+      'error', 'multi'
+    ],
+    'quotes': [
+      'error', 'single'
+    ],
+    'key-spacing': [
+      'error', { afterColon: true }
+    ],
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'object-curly-newline': [
+      'error', { 'minProperties': 2 }
+    ],
+    'object-property-newline': 'error',
+    'array-element-newline': [
+      'error',
+      {
+        'minItems': 3,
+        'multiline': true
+      }
+    ],
+    'array-bracket-newline': [
+      'error',
+      {
+        'multiline': true,
+        'minItems': 1
+      }
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        'singleline': 'never',
+        'multiline': 'never'
+      }
+    ],
+    'vue/html-indent': [
+      'error', 2
+    ],
+    'vue/script-indent': [
+      'error', 2
+    ]
+  },
+  overrides: [
+    {
+      files: '*/**/*.vue',
+      'parser': 'vue-eslint-parser',
+      'parserOptions': { 'parser': '@typescript-eslint/parser' },
     }
+  ]
 };
