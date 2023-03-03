f07dc31585279b0bb9d1d7de5b33c1566470907e
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2023-03-04 00:59:34 +0900

    refactor: dir명 변경

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: {
     tailwindcss: {
-      config: './tailwind.config.js'
+      config: '.tailwind.config'
+      //...require('./tailwind.config.js')
     },
     autoprefixer: {},
   }
